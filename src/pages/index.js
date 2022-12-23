0e3e593885664ab0b8497ca79dd0bd863eb13e61
Kein Li <keinli@macbook-air.lan>
2022-12-23 18:44:18 -0500

    updated files

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,12 @@ export default function IndexPage() {
         {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Me" />
         <Animation type="fadeUp">
+          <Section anchor="experience" heading="Experience">
+            <p>Will update this section! Currently a Software Engineering Intern @ Arlite Technologies</p>
+          </Section>
+        </Animation>
+
+        <Animation type="fadeUp">
           <Section heading="Technologies I Use">
             <img
               src="https://skillicons.dev/icons?i=py,js,java,react,nodejs,mongodb,express,postgres,tailwind,jest,git&perline=12"
@@ -32,7 +38,7 @@ export default function IndexPage() {
         <ContactSection sectionId="github" heading="Issues?" /> */}
 
         <Animation type="fadeUp">
-          <Section anchor = "opensource" heading="Open Source">
+          <Section anchor="opensource" heading="Open Source">
             <p>TBD!</p>
           </Section>
         </Animation>
